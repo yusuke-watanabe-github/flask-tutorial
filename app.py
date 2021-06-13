6539e68b013a31b7d9ab9342f878e552b35d0211
yusuke-watanabe-github <toda2020-006@iniad.org>
2021-06-13 04:37:55 +0000

    2st commit

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello world"
-		
+#    return "Hello world"
+    return render_template('index.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
